7312c7909b7502c021ab949b4bdda00f30965044
Joseph Bautista <josephbautista@Josephs-MacBook-Pro.local>
2022-12-03 21:12:04 +0800

    updated example

--- example/main.go
+++ example/main.go
@@ -16,6 +16,11 @@ func SampleCustomMiddleware(config express.Config) express.Handler {
 	}
 }
 
+func PostIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(express.GenericResponse{"message": "POST hello world"})
+}
+
 func main() {
 	app := express.New()
 
@@ -32,14 +37,11 @@ func main() {
 		json.NewEncoder(w).Encode(express.GenericResponse{"message": "GET hello world"})
 	})
 
+	app.Post("/", PostIndex)
+
 	app.Get("/messages", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(express.GenericResponse{"message": "GET messages"})
-	})
-
-	app.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(express.GenericResponse{"message": "POST hello world"})
 	})
 
 	app.Listen(5200)
